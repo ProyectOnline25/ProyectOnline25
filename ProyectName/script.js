0df1e09f3f2a1cd384d58dd9af3ba2e6f3fe2c37
ProyectOnline25 <proyectonline25@gmail.com>
2025-07-03 22:40:48 +0200

    Delete ProyectName directory

--- ProyectName/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('main-nav').querySelector('ul');
-
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
